43349d34c37e9a0aa78c19ed0673e9699351b6c3
Anders Åslund <anders.aslund@teknoir.ai>
2022-06-28 11:52:28 +0200

    Second version

--- release.sh
+++ release.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-VERSION=0.1.0
+VERSION=0.1.1
 
 ## force version in package.json to match VERSION from this script
 npm --no-git-tag-version version $VERSION --force --allow-same-version
